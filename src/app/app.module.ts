74a446f4f881926244d3142127bbe395473846b8
Michael Roque <mikeroque1@gmail.com>
2017-06-04 15:14:05 -0600

    changed header color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { MapPage } from '../pages/map/map';
+import { ListPage } from '../pages/list/list';
+import { SharePage } from '../pages/share/share';
+import { SearchPage } from '../pages/search/search';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +21,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MapPage,
+    ListPage,
+    SharePage,
+    SearchPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +37,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MapPage,
+    ListPage,
+    SharePage,
+    SearchPage
   ],
   providers: [
     StatusBar,
